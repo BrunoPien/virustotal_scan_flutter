aa22d93e912917625c81b950c81233533923aa5f
Bruno Alexandre de Souza <bruno.pien.sistemas@gmail.com>
2019-01-22 20:02:50 -0200

    Inicio
    Inicial

---
+++ lib/Screen/SendFileScreen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class SendFileScreen extends StatefulWidget {
+  _SendFileScreenState createState() => _SendFileScreenState();
+}
+
+class _SendFileScreenState extends State<SendFileScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+       child: Scaffold(
+         appBar: AppBar(
+           title: Text("Virus Total Flutter"),
+         ),
+         body: Text('AAA'),
+       ),
+    );
+  }
+}
